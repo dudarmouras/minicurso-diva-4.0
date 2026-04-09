dc9570f1a857eadcb456aacfb41cc0ae647c7dd5
IsabelleTenorio <isabelle.tenorio05@gmail.com>
2026-04-09 09:40:02 -0300

    feat: added social media buttons and changed the header

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import Blusinha from "./components/blusinha";
 import kitBlusinhas from "./assets/kit-blusinhas.png";
 import tubeTopRosa from "./assets/tube-top-rosa.png";
 import crocheVerde from "./assets/croche-verde.png";
+import instagramIcon from "./assets/Instagram_icon.png";
+import facebookIcon from "./assets/Facebook_logo.png";
+import whatsappIcon from "./assets/whatsapp_logo.png";
 import "./App.css";
 import SearchBar from "./components";
 
@@ -22,11 +25,41 @@ function App() {
   return (
     <div className="app">
       <header className="header">
-        <p className="title">uma loja de blusinhas</p>
+        <p className="title">Lojinha do &lt;div&gt;a</p>
       </header>
       <div className="content">
         <div className="search-bar-container">
           <SearchBar onChange={(e) => setSearchTerm(e.target.value)} />
+          <div className="social-bar">
+            <a
+              href="https://www.instagram.com/diva.citi/"
+              className="social-button"
+              aria-label="Instagram"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={instagramIcon} alt="Instagram" className="social-icon" />
+            </a>
+            <a
+              href="https://www.facebook.com"
+              className="social-button"
+              aria-label="Facebook"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={facebookIcon} alt="Facebook" className="social-icon" />
+            </a>
+            <a
+              href="https://wa.me/"
+              className="social-button"
+              aria-label="WhatsApp"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <img src={whatsappIcon} alt="WhatsApp" className="social-icon" />
+            </a>
+          </div>
+          
         </div>
         <div className="blusinhas-container">
           {filteredBlusinhas.map((blusinha, index) => (
